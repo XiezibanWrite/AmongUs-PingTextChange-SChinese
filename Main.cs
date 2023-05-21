c2aaf22e7d31918fee32e8a3610025f98ef4e694
XiezibanWrite <103623519+XiezibanWrite@users.noreply.github.com>
2023-05-21 11:53:08 +0800

    Update Code

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+namespace PingTextChange;
+
+[BepInPlugin(Id, ModName, VersionString)]
+[BepInProcess("Among Us.exe")]
+
+public partial class PingTextChangePlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public const string ModName = "PingTextChange";
+
+    public const string Id = "4hgroup.developer.PingTextChange";
+    // 模组版本
+    public const string VersionString = "0.0.1";
+
+
+    public override void Load()
+    {
+
+        Harmony.PatchAll();
+    }
+
+
+}
---
+++ PingTextChange.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Hanpi.PingTextChange
+{
+    //[HarmonyPriority(Priority.VeryHigh)] // to show this message first, or be overrided if any plugins do
+    [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+    public static class PingTracker_Update
+    {
+
+        [HarmonyPostfix]
+        public static void Postfix(PingTracker __instance)
+        {
+            var position = __instance.GetComponent<AspectPosition>();
+            position.DistanceFromEdge = new Vector3(3.6f, 0.1f, 0);
+            position.AdjustPosition();
+
+            __instance.text.text =
+                $"延迟: {AmongUsClient.Instance.Ping}毫秒\n";
+        }
+    }
+}
